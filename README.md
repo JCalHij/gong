8de0ad11a3bbbc3d747903bff50d2a3a4406b784
Javier Calderon Hijano <javcalhij@gmail.com>
2022-07-14 17:10:17 +0200

    Raylib example

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Pong, but in `go`.
 ## But why?
 
 Because I want to code, I want to finish a project and I want to learn some `go`.
+
+
+## Compile, Run
+
+```sh
+# Simply run this
+go run main.go
+# Create a binary file
+go build main.go
+```
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	fmt.Println("gong v.0")
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
 }
