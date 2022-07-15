1ef04fd621f690d772705e15e3877629c72c7215
Javier Calderon Hijano <javcalhij@gmail.com>
2022-07-15 16:03:56 +0200

    Moving ball, player input

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-import "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+
+	"github.com/gen2brain/raylib-go/raylib"
+)
 
 func draw_rect(rect *rl.Rectangle, color rl.Color) {
 	var x int32 = int32(rect.X)
@@ -9,6 +14,13 @@ func draw_rect(rect *rl.Rectangle, color rl.Color) {
 	var w int32 = int32(rect.Width)
 	var h int32 = int32(rect.Height)
 	rl.DrawRectangle(x, y, w, h, color)
+}
+
+func vec2_from_angle(angle float64) rl.Vector2 {
+	v := rl.Vector2{
+		X: float32(math.Cos(angle)),
+		Y: float32(math.Sin(angle))}
+	return rl.Vector2Normalize(v)
 }
 
 const PaddleWidth = 30
@@ -21,6 +33,9 @@ const TextScoreSpacing = 30
 func main() {
 	var WindowWidth int32 = 1200
 	var WindowHeight int32 = 600
+
+	var PaddleSpeed float32 = float32(WindowHeight) * 0.3 // [px/s] Paddle speed as a percentage of the screen height
+	var BallSpeed float32 = float32(WindowWidth) * 0.2
 
 	rl.InitWindow(WindowWidth, WindowHeight, "gong")
 
@@ -46,10 +61,10 @@ func main() {
 		Width:  BallWidth,
 		Height: BallHeight}
 
-	var DeltaTime float32 = 0.0
+	var BallDirection rl.Vector2 = vec2_from_angle(rand.Float64())
 
 	for !rl.WindowShouldClose() {
-		DeltaTime = rl.GetFrameTime()
+		var DeltaTime float32 = rl.GetFrameTime() // [s] frame time
 
 		/* Game Logic */
 
@@ -58,25 +73,62 @@ func main() {
 
 		LeftScoreText := fmt.Sprintf("%d", LeftScore)
 
+		/* Player movement */
+		{
+			if rl.IsKeyDown(rl.KeyW) {
+				LeftPaddle.Y -= PaddleSpeed * DeltaTime
+			}
+			if rl.IsKeyDown(rl.KeyS) {
+				LeftPaddle.Y += PaddleSpeed * DeltaTime
+			}
+		}
+
+		/* Enemy movement */
+		{
+			//TODO
+		}
+
+		/* Ball movement */
+		{
+			var BallPos rl.Vector2 = rl.Vector2{X: Ball.X, Y: Ball.Y}
+			var BallDeltaMovement rl.Vector2 = rl.Vector2{
+				X: BallSpeed * BallDirection.X * DeltaTime,
+				Y: BallSpeed * BallDirection.Y * DeltaTime}
+			var BallNewPos = rl.Vector2Add(BallPos, BallDeltaMovement)
+
+			// Collision checks
+			// Window limits
+			if BallNewPos.X+BallWidth >= float32(WindowWidth) || BallNewPos.X <= 0.0 {
+				BallDirection.X *= -1
+			}
+			if BallNewPos.Y+BallHeight >= float32(WindowHeight) || BallNewPos.Y <= 0.0 {
+				BallDirection.Y *= -1
+			}
+
+			Ball.X += BallSpeed * BallDirection.X * DeltaTime
+			Ball.Y += BallSpeed * BallDirection.Y * DeltaTime
+		}
+
 		/* Rendering */
+		{
+			rl.BeginDrawing()
+			rl.ClearBackground(rl.Black)
 
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
+			// Paddles
+			draw_rect(&LeftPaddle, rl.White)
+			draw_rect(&RightPaddle, rl.White)
+			// Ball
+			draw_rect(&Ball, rl.White)
 
-		// Paddles
-		draw_rect(&LeftPaddle, rl.White)
-		draw_rect(&RightPaddle, rl.White)
-		// Ball
-		draw_rect(&Ball, rl.White)
+			// Score
+			rl.DrawText(LeftScoreText, WindowWidth/2.0-TextScoreSpacing-RightTextWidth, 10, ScoreFontSize, rl.White)
+			rl.DrawText(RightScoreText, WindowWidth/2.0+TextScoreSpacing, 10, ScoreFontSize, rl.White)
 
-		// Score
-		rl.DrawText(LeftScoreText, WindowWidth/2.0-TextScoreSpacing-RightTextWidth, 10, ScoreFontSize, rl.White)
-		rl.DrawText(RightScoreText, WindowWidth/2.0+TextScoreSpacing, 10, ScoreFontSize, rl.White)
+			// Debug performance
+			rl.DrawText(fmt.Sprintf("%.03f ms", DeltaTime*1000), 10, 10, 25, rl.Red)
 
-		// Debug performance
-		rl.DrawText(fmt.Sprintf("%.03f ms", DeltaTime*1000), 10, 10, 25, rl.White)
-
-		rl.EndDrawing()
+			rl.EndDrawing()
+		}
 	}
 
 	rl.CloseWindow()
