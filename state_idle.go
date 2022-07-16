7edec7fe731e64f482c8e6be968ebbc261428b47
Javier Calderon Hijano <javcalhij@gmail.com>
2022-07-16 11:05:02 +0200

    Gameplay instruction texts

--- state_idle.go
+++ state_idle.go
@@ -34,4 +34,22 @@ func idle_game_render(GS *GameState) {
 	TextString := "Press SPACE to begin playing"
 	TextWidth := rl.MeasureText(TextString, InstructionsFontSize)
 	rl.DrawText(TextString, (WindowWidth-TextWidth)/2, WindowHeight-80, InstructionsFontSize, rl.White)
+
+	if GS.LeftPlayerHuman {
+		// "Move up and down with keys W and S"
+		TextString = "Move up and down"
+		rl.DrawText(TextString, 10, WindowHeight/2-PaddleHeight*2, InstructionsFontSize, rl.Gray)
+		TextString = "with keys W and S"
+		rl.DrawText(TextString, 10, WindowHeight/2-PaddleHeight*2+InstructionsFontSize, InstructionsFontSize, rl.Gray)
+	}
+
+	if GS.RightPlayerHuman {
+		// "Move up and down with arrows up and down"
+		TextString = "Move up and down"
+		TextWidth = rl.MeasureText(TextString, InstructionsFontSize)
+		rl.DrawText(TextString, WindowWidth-TextWidth-20, WindowHeight/2-PaddleHeight*2, InstructionsFontSize, rl.Gray)
+		TextString = "with arrows up and down"
+		TextWidth = rl.MeasureText(TextString, InstructionsFontSize)
+		rl.DrawText(TextString, WindowWidth-TextWidth-20, WindowHeight/2-PaddleHeight*2+InstructionsFontSize, InstructionsFontSize, rl.Gray)
+	}
 }
